7fda2f6e46db8214a1af115daa56a748e134704d
Katie Barriere <kbarriere@gmail.com>
2020-05-26 20:12:27 -0400

    Initial commit

---
+++ routes/api/profile.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const app = express();
+
+// @route  GET api/profile
+// @desc   Test route
+// @access Public
+router.get('/', (req, res) => res.send('Profile route'));
+
+module.exports = router;
