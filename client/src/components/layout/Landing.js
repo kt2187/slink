c91324fa67f7dd6c475b40bf2a4f2e1fedfaf9bc
Katie Barriere <kbarriere@gmail.com>
2020-06-03 17:20:37 -0400

    Add routes for login and registration

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 const Landing = () => {
   return (
-    <section className="landing">
-      <div className="dark-overlay">
-        <div className="landing-inner">
-          <h1 className="x-large">Slink</h1>
-          <p className="lead">
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Slink</h1>
+          <p className='lead'>
             Connecting students to educators and employers
           </p>
-          <div className="buttons">
-            <a href="register.html" className="btn btn-primary">
+          <div className='buttons'>
+            <Link to='/register' className='btn btn-primary'>
               Sign Up
-            </a>
-            <a href="login.html" className="btn btn-light">
+            </Link>
+            <Link to='/login' className='btn btn-light'>
               Login
-            </a>
+            </Link>
           </div>
         </div>
       </div>
