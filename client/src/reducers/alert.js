1e2537c9b0a2ec184804345cede7fe277e73c327
Katie Barriere <kbarriere@gmail.com>
2020-06-04 18:53:29 -0400

    Add alert component and action call

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -4,8 +4,9 @@ const initialState = [];
 
 export default function (state = initialState, action) {
   const { type, payload } = action;
-  switch (action.type) {
-    case 'SET_ALERT':
+
+  switch (type) {
+    case SET_ALERT:
       return [...state, payload];
     case REMOVE_ALERT:
       return state.filter((alert) => alert.id !== payload);
