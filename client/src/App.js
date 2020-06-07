0ee00d08daac275b85ca446cfa9ff20d093305eb
Katie Barriere <kbarriere@gmail.com>
2020-06-07 09:12:00 -0400

    Create profile component with form to add user profile

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 //Redux
 import { Provider } from 'react-redux';
@@ -16,13 +17,14 @@ import setAuthToken from './utils/setAuthToken';
 import './App.css';
 
 if (localStorage.token) {
-  setAuthToken(localStorage.token);
-}
-
-const App = () => {
-  useEffect(() => {
-    store.dispatch(loadUser());
-  }, []);
+    setAuthToken(localStorage.token);
+  }
+  
+  const App = () => {
+    useEffect(() => {
+      store.dispatch(loadUser());
+    }, []);
+    
   return (
     <Provider store={store}>
       <Router>
@@ -35,6 +37,11 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,9 +1,9 @@
+import { getCurrentProfile } from '../../actions/profile';
 import React, { Fragment, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
-import { getCurrentProfile } from '../../actions/profile';
 
 const Dashboard = ({
   getCurrentProfile,
@@ -26,7 +26,7 @@ const Dashboard = ({
       ) : (
         <Fragment>
           <p>You have not yet setup a profile, please add some info</p>
-          <Link to="create=profile" className="btn btn-primary my-1">
+          <Link to="create-profile" className="btn btn-primary my-1">
             Create Profile
           </Link>
         </Fragment>
